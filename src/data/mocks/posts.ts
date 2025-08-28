25df25325258fedb4d5735491885a10d3410c0fa
Cerebrocyber <cerebroultron@gmail.com>
2025-08-27 21:37:28 -0300

    vai

--- src/data/mocks/posts.ts
+++ src/data/mocks/posts.ts
@@ -184,22 +184,4 @@ A privacidade digital em 2024 é um cenário em constante evolução. Estar info
   },
 ];
 
-export type Post = {
-  id: string;
-  title: string;
-  slug: string;
-  status: 'Published' | 'Draft';
-  type: 'Tip' | 'Future';
-  tags: string[];
-  publishedAt: string;
-  excerpt: string;
-  seoTitle: string;
-  seoDescription: string;
-  cover: string;
-  altText: string;
-  videoUrl: string | null;
-  readingTime: number;
-  content: string;
-};
-
 
